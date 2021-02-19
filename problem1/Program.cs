9ad018198e3fd6339ead68f8be0fb492630b9318
Koichi MAKINO <k4i.makino@gmail.com>
2021-02-20 05:35:27 +0900

    add problem1

---
+++ problem1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 0;
+
+            answer = Enumerable.Range(1, 999)
+                    .Where(i => i % 5 == 0 || i % 3 == 0)
+                    .Sum();
+
+            Console.WriteLine(answer);
+        }
+    }
+}
