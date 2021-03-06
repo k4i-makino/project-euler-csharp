b46192a8d785b937eec9913b2de019c2d08874b0
Koichi MAKINO <k4i.makino@gmail.com>
2021-03-07 04:52:18 +0900

    add problem2

---
+++ problem2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fib = new int[3] {1, 2, 3};
+            var answer = 2;
+            while (fib[2] < 4000000)
+            {
+                if (fib[2] % 2 == 0)
+                {
+                    answer += fib[2];
+                }
+                fib[0] = fib[1];
+                fib[1] = fib[2];
+                fib[2] = fib[0] + fib[1];
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
